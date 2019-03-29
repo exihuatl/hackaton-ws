5214ca7da875e730b415bc2bfc7160184729f98d
Patryk Sadza <exihuatl@gmail.com>
2019-03-29 09:22:02 +0100

    add unique id

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import uuid from 'uuid'
 import './App.css'
 
-const ws = new WebSocket('ws://localhost:8080')
+const ws = new WebSocket('ws://192.168.6.205:8080')
 
 ws.onopen = function open() {
   ws.send('something')
@@ -14,8 +15,24 @@ ws.onmessage = function incoming(data) {
 const arr = [0, 1, 2, 3, 5, 8, 13, 21, 34, 99]
 
 function App() {
+  const [id, setId] = useState()
+
+  useEffect(() => {
+    setId(localStorage.getItem('uuid'))
+
+    if (!id) {
+      const uid = uuid()
+      setId(uid)
+      localStorage.setItem('uuid', uid)
+    }
+  }, [])
+
   const onClick = num => () => {
-    ws.send(num)
+    const payload = {
+      [id]: num
+    }
+    ws.send(JSON.stringify(payload))
+    console.log('payload', payload)
   }
   return (
     <div className="app">
